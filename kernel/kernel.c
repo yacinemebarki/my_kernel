c7101e1b30596f7d6801c8ddcb45344c56da5404
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-06-19 15:05:26 +0100

    add clear function and move cursor function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,29 +4,52 @@ void print(char c, int i){
     char *line = (char *)0xB8000;
     line[2 * i] = c;
     line[2 * i + 1] = 0x0F;
+    move(i+1);
+}
+
+void clear(int i){
+    char *line = (char *)0xB8000;
+    line[2 * i] = ' ';
+    move(i);
+}
+
+static inline void outb(unsigned short port, unsigned char value){
+    __asm__ volatile("outb %0, %1"::"a"(value),"Nd"(port));
+}
+
+void move(unsigned short pos){
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, pos & 0x0FF);
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (pos >> 8) & 0xFF);
 }
 
 void kernel(){
     int i = 640;
     int space = 80;
+    int j = 0;
     while (1){
         char c = read();
         if(c != 0){
             if(c == '\n'){
-                //move cursor function will be add 
                 i = i + space;
+                i = i - j;
+                move(i);
+                j = 0;
             }
             else if(c == ' '){
-                //mov cursor function will be add
                 i++;
+                j++;
+                move(i+1);
             }
             else if(c == '\b'){
-                //clear_char function will be add
                 i--;
+                clear(i);  
             }
             else {
                 print(c, i);
                 i++;
+                j++;
             }
         }
     }

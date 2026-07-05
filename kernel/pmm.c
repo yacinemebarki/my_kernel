6636d2ad11c04ae0ed089fcf76e8ef7112725567
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-07-05 14:32:50 +0100

    add allocate and free function

--- kernel/pmm.c
+++ kernel/pmm.c
@@ -8,9 +8,11 @@ typedef struct {
     unsigned int type;
     unsigned int acpi;
 } Memory_Map;
+
+uint16_t *entry = (uint16_t *)0x0500;
+Memory_Map *map = (Memory_Map *)0x504;
+
 void search(int i, int j){
-    uint16_t *entry = (uint16_t *)0x500;
-    Memory_Map *map = (Memory_Map *)0x504;
     print_string("Entries: ", &i, &j);
     print_number(*entry, &i);
     i = i + 80;
@@ -26,5 +28,47 @@ void search(int i, int j){
         print_string("Type: ", &i, &j);
         print_number(type, &i);
         i = i + 80;
+    }
+}
+
+void allocate(unsigned long long size){
+    
+    int best = -1;
+
+    for(int k = 0; k < *entry; k++){
+        if(map[k].length >= size && map[k].type == 1){
+            if(best == -1 || map[best].length > map[k].length){
+                best = k;
+            }
+        }
+    }
+    if( best == -1){
+        return;
+    }
+
+    unsigned long long remain = map[best].length - size;
+    
+    if(remain > 0){
+        for(int k = *entry; k > best; k--){
+            map[k] = map[k -1];
+        }
+        map[best + 1].base = map[best].base + size;
+        map[best + 1].length = remain;
+        map[best + 1].type = 1;
+        map[best + 1].acpi = map[best].acpi;
+        (*entry)++;
+    }
+    map[best].length = size;
+    map[best].type = 2;
+
+}
+
+void free(unsigned long long add){
+
+    for(int k = 0; k < *entry; k++){
+        if(map[k].base == add){
+            map[k].type = 1;
+            return;
+        }
     }
 }
